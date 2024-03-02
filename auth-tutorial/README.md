805bfb379eb15d055710f2bbf4220517eb693876
kacper.pichola <kacper.pichola@amrest.eu>
2024-03-02 21:56:46 +0100

    Starting the tutorial

---
+++ auth-tutorial/README.md
@@ -0,0 +1,5 @@
+Following the tutorial:
+https://www.youtube.com/watch?v=1MTyCvS05V4&t=201s&ab_channel=CodeWithAntonio
+
+Last stop:
+1:22:58
---
+++ auth-tutorial/app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { LoginForm } from "@/components/auth/login-form";
+const LoginPage = () => {
+  return <LoginForm />;
+};
+
+export default LoginPage;
